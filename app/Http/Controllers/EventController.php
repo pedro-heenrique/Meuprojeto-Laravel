52479011f43aba469fdf08a22b6ee84a3591012a
Pedro Henrique Alves <ped_rohenrrique@hotmail.com>
2023-12-08 08:55:15 -0300

    Atualizado 08/12

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Event; // Certifique-se de importar o modelo Event
+use App\Models\Event; 
 
 class EventController extends Controller
 {
